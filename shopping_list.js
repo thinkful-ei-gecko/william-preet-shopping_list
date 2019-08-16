e78e02b057fa528848313a9299facfbe979ba3eb
William Bae <wbae912@gmail.com>
2019-08-15 18:11:18 -0700

    added js script to html and completed shopping list

--- shopping_list.js
+++ shopping_list.js
@@ -1,11 +1,11 @@
 'use strict'
-function enterItem(){
-    $('#js-shopping-list-form').submit(event => {
-        event.preventDefault();
-        let userShoppingItem = $('#shopping-list-entry').val();
+function enterItem() {
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    let userShoppingItem = $('#shopping-list-entry').val();
         
-        $('.shopping-list').append(`
-        <li>
+    $('.shopping-list').append(
+      `<li>
         <span class="shopping-item">${userShoppingItem}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
@@ -15,19 +15,36 @@ function enterItem(){
             <span class="button-label">delete</span>
           </button>
         </div>
-      </li>`);
-
-
-    });
-
+      </li>`
+    );  
+  });
 }
 
-function checkItems(){
+/*The function below WORKS, in the sense that it checks off the EXISTING items on the page. However, for newly created items,
+they will not be initially bound so event listening will not apply. That is why we must create the event listening on the PARENT
+('ul')*/
 
+// function checkItems() {
+//   $('li').on('click', function() {
+//     $(this).toggleClass('shopping-item__checked');
+//   });
+// }
+
+//This is the correct function using event delegation:
+function checkItems() {
+  $('ul').on('click', '.shopping-item-toggle', function () {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked')
+  });
 }
 
-function removerItems(){
 
+
+function removeItems() {
+  $('ul').on('click', '.shopping-item-delete', function() {
+    $(this).closest('li').remove();
+  });
 }
 
 $(enterItem);
+$(checkItems);
+$(removeItems);
